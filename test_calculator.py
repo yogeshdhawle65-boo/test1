0b0070971853e492f2bd3dcb3256631a595a0bb5
yogesh dhawle <yogeshdhawle65@gmail.com>
2026-09-02 18:41:57 +0530

    Ad

--- test_calculator.py
+++ test_calculator.py
@@ -12,11 +12,11 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 4), 3)
 
     def test_subtract_numbers(self):
-        self.assertEqual(subtract(10, 4), 6)
-
-    def test_multiply_numbers(self):
-        self.assertEqual(multiply(3, 5), 15)
-
-
-if __name__ == "__main__":
-    unittest.main()
+          self.assertEqual(subtract(10, 4), 6)
+  
+      def test_multiply_numbers(self):
+          self.assertEqual(multiply(3, 5), 15)
+  
+  
+  if __name__ == "__main__":
+      unittest.main()
