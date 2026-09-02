e6d3cd61c8d798c7e53bd188148f647421e89b4a
yogesh dhawle <yogeshdhawle65@gmail.com>
2026-09-02 15:44:48 +0530

    content

---
+++ calculatotr.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
+    print("10 - 4 =", subtract(10, 4))
+    print("3 * 5 =", multiply(3, 5))
